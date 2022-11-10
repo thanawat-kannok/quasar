edb72b75d88d3abb174151475d708c6b4fab7da9
thanawat-kannok <thanawat.ko@rmuti.ac.th>
2022-11-10 13:09:37 +0700

    add

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,134 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title> จัดการผู้ใช้ / กลุ่มผู้ใช้</q-toolbar-title>
+
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-person-circle"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+            <path
+              fill-rule="evenodd"
+              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-caret-down-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"
+            />
+          </svg>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+  <q-body> </q-body>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/layouts/login.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-page-container class="bg-grey-2">
+      <q-page padding class="row items-center justify-center">
+        <div class="row full-width">
+          <div class="col-md-8 offset-md-2 col-xs-12 q-pl-md q-pr-md q-pt-sm">
+            <q-card flat class="bg-white text-black">
+              <div class="row">
+                <div class="col-md-12 col-xs-12">
+                  <div class="q-pa-md">
+                    <div
+                      class="text-h6 q-pb-md text-blue-8 text-center text-weight-bolder"
+                    >
+                      เข้าสู่ระบบ
+                    </div>
+                    <q-form
+                      class="q-gutter-md"
+                      @submit="onSubmit"
+                      @reset="onReset"
+                    >
+                      <q-input
+                        filled
+                        v-model="name"
+                        label="Username"
+                        lazy-rules
+                      />
+
+                      <q-input
+                        filled
+                        type="password"
+                        v-model="password"
+                        label="Password"
+                        lazy-rules
+                      />
+
+                      <div class="">
+                        <q-btn label="Login" type="submit" color="primary" />
+                        <q-btn
+                          label="Reset"
+                          type="reset"
+                          color="primary"
+                          flat
+                          class="q-ml-sm"
+                        />
+                      </div>
+                    </q-form>
+                  </div>
+                </div>
+              </div>
+            </q-card>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "q-Login",
+
+  data() {
+    return {
+      name: null,
+      password: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("click submit");
+      this.$router.push("/main");
+    },
+    onReset() {
+      this.name = null;
+      this.password = null;
+    },
+  },
+});
+</script>
